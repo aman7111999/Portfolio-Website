ff96fa3e61d82cdb1bf146aea74fc23563692b22
aman7111999 <56710099+aman7111999@users.noreply.github.com>
2026-09-02 01:08:42 +0530

    Place dark portfolio screen behind center screen

--- src/components/projects/PortfolioAnalysisCardVisual.tsx
+++ src/components/projects/PortfolioAnalysisCardVisual.tsx
@@ -98,14 +98,14 @@ export function PortfolioAnalysisCardVisual({
         {REAL_SCREENS.map((screen, index) => {
           const position = hero
             ? [
-                "left-[1%] top-[19%] h-[77%] -rotate-[4deg] opacity-72",
-                "left-[25%] top-[1%] h-[96%] -rotate-[0.5deg]",
-                "left-[56%] top-[16%] h-[81%] rotate-[3deg] opacity-90",
+                "left-[1%] top-[19%] z-20 h-[77%] -rotate-[4deg] opacity-72",
+                "left-[25%] top-[1%] z-30 h-[96%] -rotate-[0.5deg]",
+                "left-[48%] top-[16%] z-10 h-[81%] rotate-[3deg] opacity-86",
               ][index]
             : [
-                "left-[1%] top-[21%] h-[73%] -rotate-[4deg] opacity-70",
-                "left-[25%] top-[2%] h-[93%] -rotate-[0.5deg]",
-                "left-[56%] top-[18%] h-[78%] rotate-[3deg] opacity-88",
+                "left-[1%] top-[21%] z-20 h-[73%] -rotate-[4deg] opacity-70",
+                "left-[25%] top-[2%] z-30 h-[93%] -rotate-[0.5deg]",
+                "left-[48%] top-[18%] z-10 h-[78%] rotate-[3deg] opacity-84",
               ][index];
 
           return (
@@ -129,9 +129,9 @@ export function PortfolioAnalysisCardVisual({
       <div className="absolute bottom-[1%] left-[40%] right-[-8%] top-[18%] z-10 sm:hidden">
         {REAL_SCREENS.map((screen, index) => {
           const position = [
-            "left-0 top-[19%] h-[68%] -rotate-[5deg] opacity-64",
-            "left-[23%] top-0 h-[90%] -rotate-[1deg]",
-            "left-[54%] top-[16%] h-[73%] rotate-[4deg] opacity-82",
+            "left-0 top-[19%] z-20 h-[68%] -rotate-[5deg] opacity-64",
+            "left-[23%] top-0 z-30 h-[90%] -rotate-[1deg]",
+            "left-[46%] top-[16%] z-10 h-[73%] rotate-[4deg] opacity-78",
           ][index];
           return (
             <figure
